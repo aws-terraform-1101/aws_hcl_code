8efa75bf36fbb9d0b3d63844646a35a918cef562
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 23:09:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ resource "aws_instance" "ec2_example" {
     }
 }
 
+resource "aws_instance" "ec2_e" {
+    ami = "ami-0767046d1677be5a0"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "Terraform EC2 1101 did slack work"
+    }
+}
+
 resource "aws_instance" "ec2" {
     ami = "ami-0767046d1677be5a0"  
     instance_type = "t2.micro" 
