4038a7de628f6023d82718ab68db2032aa9341b0
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 22:10:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "app_server" {
 
 
 resource "aws_instance" "ec2" {
-  ami = "ami-019f0583716f0aec4" 
+  ami = "ami-0b9064170e32bde34" 
   instance_type = "t2.micro"
   tags = {
     Name = "visualscrapy"
