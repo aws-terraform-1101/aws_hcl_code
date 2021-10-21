c134deb54f9861edb12b88964e9224e5bb8775c3
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-21 09:41:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,6 @@ resource "aws_instance" "ec2" {
     ami = "ami-0767046d1677be5a0"  
     instance_type = "t2.micro" 
     tags = {
-        Name = "Terraform EC2 1101 SECONF"
+        Name = "Terraform EC2 1101 SECOND"
     }
 }
