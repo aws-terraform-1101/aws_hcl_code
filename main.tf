d90d70cdbfd4f413a4202aa51135278788d59919
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 22:51:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "ec2_example" {
     }
 }
 
-resource "aws_instance" "ec2_example" {
+resource "aws_instance" "ec2" {
     ami = "ami-0767046d1677be5a0"  
     instance_type = "t2.micro" 
     tags = {
