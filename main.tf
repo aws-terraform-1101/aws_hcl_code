22aa773a825aa7d08da9f3ca46ef6500376fcb81
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 22:47:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-west-1"
 } 
 
-resource "aws_instance" "ec2_example22" {
+resource "aws_instance" "ec2_example" {
     ami = "ami-034bf8489816bea4d"  
     instance_type = "t2.micro" 
     tags = {
