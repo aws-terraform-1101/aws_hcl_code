379f4038fe226106288811d923a8ac8a48cd04d3
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 22:41:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region     = "us-west-1"
 } 
 
+resource "aws_instance" "ec2_example22" {
+    ami = "ami-0767046d1677be5a0"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "11011"
+    }
+}
 resource "aws_instance" "ec2_example" {
     ami = "ami-0767046d1677be5a0"  
     instance_type = "t2.micro" 
     tags = {
-        Name = "Terraform EC2"
+        Name = "new test here"
     }
 }
+
