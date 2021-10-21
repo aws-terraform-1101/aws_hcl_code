17288e0f34c197765b93ae9e6a5169274fd77eea
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-20 22:46:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,3 @@ resource "aws_instance" "ec2_example22" {
         Name = "11011"
     }
 }
-resource "aws_instance" "ec2_example" {
-    ami = "ami-034bf8489816bea4d"  
-    instance_type = "t2.micro" 
-    tags = {
-        Name = "new test here"
-    }
-}
-
